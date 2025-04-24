67a7a0bb1a34f167483205df0b6f9ad0e074f889
lmmv9411 <luismmv9411@gmail.com>
2025-04-24 18:27:12 -0500

    ajustes names

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
+import AddMovementModal from './components/AddMovementModal.vue';
 import Balance from './components/Balance.vue';
-import Home from './components/Home.vue';
-import MovementForm from './components/MovementForm.vue';
 import MovementList from './components/MovementList.vue';
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-100 p-6">
-    <Home />
+    <AddMovementModal />
     <Balance />
     <MovementList />
   </div>
