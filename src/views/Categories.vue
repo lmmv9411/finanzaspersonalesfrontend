ccc8571acb32fcb27967520b7f162092434c80ac
lmmv9411 <luismmv9411@gmail.com>
2025-07-02 11:24:58 -0500

    categorias

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,60 +1,66 @@
 <template>
 
-    <div class="pt-4 flex flex-col gap-8 px-4">
-        <form
-              @submit.prevent="createCategory"
-              class="shadow-md p-2 rounded dark:bg-gray-800 bg-slate-100 flex flex-wrap gap-2 dark:text-gray-300 items-start m-auto">
+    <div class="pt-4 flex flex-col gap-4 px-4 items-end">
 
-            <BaseInput v-model.trim="name" placeholder="Nombre de categoria" />
+        <div @click="showForm = !showForm" class="rounded-full bg-indigo-600 p-3 text-white cursor-pointer">
+            <template v-if="!showForm">
+                <PlusIcon class="w-7 h-7" />
+            </template>
+            <template v-else>
+                <EyeSlashIcon class="w-7 y-7" />
+            </template>
+        </div>
 
-            <div class="flex gap-2">
-                <BaseButton type="submit">{{ isUpdate ? 'Actualizar' : 'Crear' }}</BaseButton>
-                <BaseButton color="danger" v-show="isUpdate" @click="reset">Cancelar</BaseButton>
-                <BaseButton
-                            color="secondary"
-                            @click="showIconPicker = !showIconPicker"
-                            class="flex items-center gap-2 p-2 border rounded dark:border-gray-500">
+        <Transition name="slide-categorie">
+            <form v-if="showForm" @submit.prevent="createCategory"
+                  class="shadow-md p-4 rounded dark:bg-gray-800 bg-slate-100 flex flex-wrap gap-2 dark:text-gray-300 items-start m-auto">
 
-                    <div
-                         v-if="selectedIcon.icon"
-                         class="rounded-full p-2 w-8 h-8 flex items-center justify-center transition-all duration-300 hover:scale-110 cursor-pointer shadow-xl"
-                         :class="[getRandomBgColor(selectedIcon.icon ?? '')]">
-                        <Icon
-                              :icon="selectedIcon.icon"
-                              class="w-6 h-6 text-white" />
-                    </div>
+                <BaseInput v-model.trim="name" placeholder="Nombre de categoria" />
 
-                    <span class="dark:text-gray-100">{{ selectedIcon.name || 'Icono' }}</span>
-                </BaseButton>
-            </div>
-        </form>
+                <div class="flex gap-2">
+                    <BaseButton type="submit">{{ isUpdate ? 'Actualizar' : 'Crear' }}</BaseButton>
+                    <BaseButton color="secondary" @click="showIconPicker = !showIconPicker"
+                                class="flex items-center gap-2 p-2 border rounded dark:border-gray-500">
 
-        <IconPickerSolid
-                         v-if="showIconPicker"
-                         @selected="handleIconSelected"
-                         @close="showIconPicker = false" />
+                        <div v-if="selectedIcon.icon"
+                             class="rounded-full p-2 w-8 h-8 flex items-center justify-center transition-all duration-300 hover:scale-110 cursor-pointer shadow-xl"
+                             :class="[getRandomBgColor(selectedIcon.icon ?? '')]">
+                            <Icon :icon="selectedIcon.icon" class="w-6 h-6 text-white" />
+                        </div>
 
-        <div class="container max-w-xs mx-auto flex gap-4 flex-col">
+                        <span class="dark:text-gray-100">{{ selectedIcon.name || 'Icono' }}</span>
+                    </BaseButton>
+                    <BaseButton color="danger" v-show="isUpdate" @click.stop="handleCancel">Cancelar</BaseButton>
+                </div>
+            </form>
+        </Transition>
+
+        <IconPickerSolid v-if="showIconPicker" @selected="handleIconSelected" @close="showIconPicker = false" />
+
+        <div class="container max-w-lg mx-auto grid grid-cols-4 gap-4">
             <div v-for="categorie in categoriesStore.categories" :key="categorie.id">
-                <div
-                     class="text-xl border gap-4 border-gray-200 dark:border-gray-600 rounded-lg flex justify-between p-4 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300 shadow-sm">
+                <div @click.stop="handleClick(categorie.id)"
+                     class="rounded-lg dark:border-gray-600 gap-1 flex flex-col justify-between items-center p-4 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300 shadow-sm">
+
                     <div :class=[getRandomBgColor(categorie.icon)]
-                         class="rounded-full p-2 w-10 h-10 flex items-center justify-center transition-all duration-300 group-hover:scale-110 cursor-pointer shadow-xl">
+                         class="text-xl rounded-full p-2 w-10 h-10 flex items-center justify-center transition-all duration-300 group-hover:scale-110 cursor-pointer shadow-xl">
                         <Icon :icon="categorie.icon" class="text-white" />
                     </div>
-                    <span>{{ categorie.name }}</span>
-                    <div class="flex gap-2">
-                        <button @click="deleteCategory(categorie.id)"
-                                class="text-red-500 hover:text-red-700 cursor-pointer"
-                                title="Eliminar categoria">
-                            <TrashIcon class="h-5 w-5" />
-                        </button>
-                        <button @click="onUpdateCategory(categorie)"
-                                class="text-blue-500 hover:text-blue-700 cursor-pointer"
-                                title="Editar categoria">
-                            <PencilSquareIcon class="h-5 w-5" />
-                        </button>
-                    </div>
+
+                    <span class="text-xs">{{ categorie.name }}</span>
+
+                    <Transition name="slide-categorie">
+                        <div class="flex gap-2" v-if="categorie.id === categorieActive">
+                            <button @click.stop="deleteCategory(categorie.id)"
+                                    class="text-red-500 hover:text-red-700 cursor-pointer" title="Eliminar categoria">
+                                <TrashIcon class="h-5 w-5" />
+                            </button>
+                            <button @click.stop="handleUpdate(categorie)"
+                                    class="text-blue-500 hover:text-blue-700 cursor-pointer" title="Editar categoria">
+                                <PencilSquareIcon class="h-5 w-5" />
+                            </button>
+                        </div>
+                    </Transition>
                 </div>
             </div>
         </div>
@@ -63,7 +69,7 @@
 </template>
 
 <script setup>
-import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/16/solid';
+import { PencilSquareIcon, TrashIcon, PlusIcon, EyeSlashIcon } from '@heroicons/vue/16/solid';
 import { Icon } from '@iconify/vue';
 import IconPickerSolid from '../components/IconPickerSolid.vue';
 import BaseButton from '../components/ui/BaseButton.vue';
@@ -71,8 +77,30 @@ import BaseInput from '../components/ui/BaseInput.vue';
 import { useCategories } from '../composables/categories';
 import { useCategorieStore } from '../stores/categoriesStore';
 import { getRandomBgColor } from './configs/icons';
+import { ref } from 'vue';
 
 const categoriesStore = useCategorieStore()
+
+const categorieActive = ref(null)
+const showForm = ref(false)
+
+const handleClick = (idCategorie) => {
+    if (categorieActive.value === idCategorie) {
+        categorieActive.value = null
+    } else {
+        categorieActive.value = idCategorie
+    }
+}
+
+const handleUpdate = (categorie) => {
+    showForm.value = true
+    onUpdateCategory(categorie)
+}
+
+const handleCancel = () => {
+    showForm.value = false
+    reset()
+}
 
 const {
     name,
@@ -87,3 +115,16 @@ const {
 } = useCategories();
 
 </script>
+
+<style scoped>
+.slide-categorie-enter-active,
+.slide-categorie-leave-active {
+    transition: all 0.3s ease-in-out;
+}
+
+.slide-categorie-enter-from,
+.slide-categorie-leave-to {
+    opacity: 0;
+    transform: translateY(8px);
+}
+</style>
